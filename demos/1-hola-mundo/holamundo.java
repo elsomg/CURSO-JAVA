d7711c6ffac7e8b3ef4c1aae7ad7615b6345533d
Guillermo Elsom <guillerm03lsom@gmail.com>
2025-06-04 09:55:57 +0200

    Modificado

--- demos/1-hola-mundo/holamundo.java
+++ demos/1-hola-mundo/holamundo.java
@@ -4,9 +4,9 @@ public class holamundo {
     public static void main(String[] args) {
         //Aqui hay que poner el codigo
         String saludo = "hola mundo";
-        System.out.println("------");
+        System.out.println("----------prueba-");
         System.out.println(saludo);
-        System.out.println("------");
+        System.out.println("-----------");
     }
 
 }
