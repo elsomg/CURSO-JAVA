eb8fda178a8e0ea4a3854f921ff8e36c01beaaa4
Guillermo Elsom <guillerm03lsom@gmail.com>
2025-06-11 13:54:41 +0200

    ejercicios

---
+++ demos/9-operators/tienda.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tienda {
+   
+    public static void main(String[] args) {
+        Scanner scanner1=new Scanner(System.in);
+        int packets=scanner1.nextInt();
+        System.out.println("Ingresa la cantidad de paquetes");
+        double packetprice=20.0;
+    
+        
+
+        }
+
+    }
+
