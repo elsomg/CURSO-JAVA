ed10f1bb459babfe0d5f3b40cfb7222464aa01d5
Guillermo Elsom <guillerm03lsom@gmail.com>
2025-06-26 11:36:34 +0200

    proyecto

---
+++ demos/ejercicios/metodonumeros.java
@@ -0,0 +1,15 @@
+package demos.ejercicios;
+
+public class metodonumeros {
+    public static void main(String[] args) {
+
+        int resultado=sumar(5,2);
+        System.out.println(resultado);
+
+    }
+    public static int sumar(int a, int b){
+        return a+b;
+    }
+        
+    
+}
