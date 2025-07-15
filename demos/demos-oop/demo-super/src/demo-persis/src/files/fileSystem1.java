1002e423da315f610c10210e52d2642a0104a674
Guillermo Elsom <guillerm03lsom@gmail.com>
2025-07-15 11:55:18 +0200

    maven

---
+++ demos/demos-oop/demo-super/src/demo-persis/src/files/fileSystem1.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.Arrays;
+
+
+public class fileSystem1 {
+    static void listFiles(String pathName) {
+        File file = new File(pathName);
+        String[] list = file.list();
+        for (String element : list) {
+            char Type = getType(pathName + " / " + element);
+            System.out.println(type + " " + element);
+        }
+        
+
+    }
+
+    static char getType(String pathName) {
+        File file = new File(pathName);
+        return file.isDirectory() ? "D" : "F";
+    }
+
+    public static void main(String[] args) {
+        String pathName = "demo-persis/resources";
+        listFiles(pathName);
+    }
+}
---
+++ demos/demos-oop/demo-super/src/demo-persis/src/files/fileSystem2.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class fileSystem2 {
+public static void main(String[] args) {
+    
+}
